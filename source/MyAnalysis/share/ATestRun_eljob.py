c761fd8cc6f313eeb2eb0a2863d06aa2fce38623
Paul Dittrich <paul.dittrich@mailbox.tu-dresden.de>
2025-06-10 15:15:48 +0200

    format job config, add event number parameter

--- source/MyAnalysis/share/ATestRun_eljob.py
+++ source/MyAnalysis/share/ATestRun_eljob.py
@@ -6,17 +6,40 @@ import os
 # Read the submission directory as a command line argument. You can
 # extend the list of arguments with your private ones later on.
 import argparse
+
 parser = argparse.ArgumentParser()
-parser.add_argument('-c', '--config-path', dest='configPath',
-        action='store', type=str, required=True,
-        help='Path to the directory containing samples to be processed.')
-parser.add_argument( '-s', '--submission-dir', dest='submitDir',
-        action='store', type=str, default='submitDir',
-        help='Submission directory for EventLoop')
+parser.add_argument(
+    "-c",
+    "--config-path",
+    dest="configPath",
+    action="store",
+    type=str,
+    required=True,
+    help="Path to the directory containing samples to be processed.",
+)
+parser.add_argument(
+    "-s",
+    "--submission-dir",
+    dest="submitDir",
+    action="store",
+    type=str,
+    default="submitDir",
+    help="Submission directory for EventLoop",
+)
+parser.add_argument(
+    "-e",
+    "--event-limit",
+    dest="eventLimit",
+    action="store",
+    type=int,
+    default=500,
+    help="Maximum number of events to process. Use -1 for no limit.",
+)
 options = parser.parse_args()
 
 # Set up (Py)ROOT.
 import ROOT
+
 ROOT.xAOD.Init().ignore()
 
 # Set up the SampleHandler object to handle the input files
@@ -24,37 +47,38 @@ sh = ROOT.SH.SampleHandler()
 
 # Set the name of the tree in our files in the xAOD the TTree
 # containing the EDM containers is "CollectionTree"
-sh.setMetaString( 'nc_tree', 'CollectionTree' )
+sh.setMetaString("nc_tree", "CollectionTree")
 
 # Use SampleHandler to get the sample from the defined location
 sample = ROOT.SH.SampleLocal("dataset")
 for filename in os.listdir(options.configPath):
     sample.add(os.path.join(options.configPath, filename))
-sh.add (sample)
+sh.add(sample)
 
 # Print information about the sample
 sh.printContent()
 
 # Create an EventLoop job.
 job = ROOT.EL.Job()
-job.sampleHandler( sh )
-job.options().setDouble( ROOT.EL.Job.optMaxEvents, -1 )
-job.options().setString( ROOT.EL.Job.optSubmitDirMode, 'unique-link')
+job.sampleHandler(sh)
+job.options().setDouble(ROOT.EL.Job.optMaxEvents, options.eventLimit)
+job.options().setString(ROOT.EL.Job.optSubmitDirMode, "unique-link")
 
 # Create the algorithm's configuration.
 from AnaAlgorithm.DualUseConfig import createAlgorithm
-alg = createAlgorithm ( 'MyxAODAnalysis', 'AnalysisAlg' )
+
+alg = createAlgorithm("MyxAODAnalysis", "AnalysisAlg")
 
 # Add our algorithm to the job
-job.algsAdd( alg )
+job.algsAdd(alg)
 
 # Later on we'll add some configuration options for our job here
 
 # Add output stream
-job.outputAdd(ROOT.EL.OutputStream('ANALYSIS'))
+job.outputAdd(ROOT.EL.OutputStream("ANALYSIS"))
 
 # Run the job using the direct driver.
 driver = ROOT.EL.DirectDriver()
-driver.submit( job, options.submitDir )
+driver.submit(job, options.submitDir)
 
 # Do not add anything here!!!
